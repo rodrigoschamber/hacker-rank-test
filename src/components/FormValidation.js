9cd6f60dd726a65f80554e3aa3bd1eac6bd062fc
Rodrigo Schamber <rodrigoschamber@gmail.com>
2021-02-27 11:18:50 -0300

    [ADD] Added Form Validation.

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import {useForm} from 'react-hook-form'
 
 export default function FormValidation(){
-    return <h2>Form Validation</h2>
+    const {register, handleSubmit} = useForm()
+    const onSubmit = (data) => alert(JSON.stringify(data))
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h2>Form Validation</h2>
+            <input name="firstName" ref={register({required: true})} placeholder="First name" />
+            <input name="lastName" ref={register({required: true})} placeholder="Last name" />
+            <select name="category" ref={register({required: true})}>
+                <option value="">Select...</option>
+                <option value="A">Category A</option>
+                <option value="B">Category B</option>
+            </select>
+            <input type="submit" />
+        </form>
+    )
 }
