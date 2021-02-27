a008c5b8a399bcc462892e401c45e35ef982c2f7
Rodrigo Schamber <rodrigoschamber@gmail.com>
2021-02-27 12:12:31 -0300

    [ADD] Added patterns to the form validation.

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -2,13 +2,15 @@ import React from 'react'
 import {useForm} from 'react-hook-form'
 
 export default function FormValidation(){
-    const {register, handleSubmit} = useForm()
+    const {register, errors, handleSubmit} = useForm()
     const onSubmit = (data) => alert(JSON.stringify(data))
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <h2>Form Validation</h2>
-            <input name="firstName" ref={register({required: true})} placeholder="First name" />
-            <input name="lastName" ref={register({required: true})} placeholder="Last name" />
+            <input name="firstName" ref={register({required: true, pattern: /^[A-Za-z]+$/i})} placeholder="First name" />
+            {errors.firstName && "First name is required."}
+            <input name="lastName" ref={register({required: true, pattern: /^[A-Za-z]+$/i})} placeholder="Last name" />
+            {errors.lastName && "Last name is required."}
             <select name="category" ref={register({required: true})}>
                 <option value="">Select...</option>
                 <option value="A">Category A</option>
